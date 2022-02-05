28ff030fc60c1bd2a0477d5159b569d40e9e7565
Lemmy Mwaura <lemminton@gmail.com>
2022-02-05 23:24:48 +0300

    Added more dependancies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -9,6 +11,7 @@ import { ButtonComponent } from './components/button/button.component';
 import { QuoteComponent } from './components/quote/quote.component';
 import { FormComponent } from './components/form/form.component';
 import { QuoteDetailComponent } from './components/quote-detail/quote-detail.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +25,9 @@ import { QuoteDetailComponent } from './components/quote-detail/quote-detail.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
