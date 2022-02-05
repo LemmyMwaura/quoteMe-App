780c048c5f8682c641c030812142d43d88b65e2a
Lemmy Mwaura <lemminton@gmail.com>
2022-02-06 01:07:46 +0300

    Quote added

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { QuoteInterface } from 'src/app/Interface-s/quote-interface';
 
 @Component({
-  selector: 'app-quote',
+  selector: 'app-quotes',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
 export class QuoteComponent implements OnInit {
+  quotes: QuoteInterface[] = [
+    {
+      message:
+        ' “Live as if you were to die tomorrow. Learn as if you were to live forever.” ',
+      mostLikes: false,
+      author: 'Mahatma Gandhi',
+      votes: 0,
+    },
+    {
+      message: ' “That which does not kill us makes us stronger.” ',
+      mostLikes: false,
+      author: 'Friedrich Nietzsche',
+    },
+    {
+      message:
+        ' “We must not allow other people’s limited perceptions to define us.” ',
+      mostLikes: false,
+      author: ' Virginia Satir',
+    },
+    {
+      message: '  “Do what you can, with what you have, where you are.” ',
+      mostLikes: false,
+      author: 'Theodore Roosevelt',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
