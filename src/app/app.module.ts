e7cf7b89c46f390a3d7a419a8a6faa3fff869882
Lemmy Mwaura <lemminton@gmail.com>
2022-02-05 20:42:49 +0300

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { FormComponent } from './components/form/form.component';
+import { QuoteDetailComponent } from './components/quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
+    QuoteComponent,
+    FormComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
