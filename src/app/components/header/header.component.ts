6fa3e02b78b52399d8ebdaa3cf3f835d2e33f037
Lemmy Mwaura <lemminton@gmail.com>
2022-02-06 11:34:14 +0300

    button component was refactored therefore header component had to be updated

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title = 'QuoteMeApp'
+  Quote = faQuoteRight
 
   constructor() { }
 
