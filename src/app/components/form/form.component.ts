cfbb0b07681f495d0c78c0a34ac8706bc632fb43
Lemmy Mwaura <lemminton@gmail.com>
2022-02-06 18:14:04 +0300

    form added

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { QuoteInterface } from 'src/app/Interface-s/quote-interface';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  newQuote = new QuoteInterface(" ", " ")
+
+  @Output() onAddQuote: EventEmitter<QuoteInterface> = new EventEmitter()
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSubmit(){
+    if(!this.newQuote.message){
+      alert('Please add Quote')
+    }
+    this.onAddQuote.emit(this.newQuote)
   }
 
+  ngOnInit(): void {
+  }
 }
