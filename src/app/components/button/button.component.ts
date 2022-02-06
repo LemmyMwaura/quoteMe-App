006838c9ef5b04726407fd6590924b1e4de247c4
Lemmy Mwaura <lemminton@gmail.com>
2022-02-06 11:32:50 +0300

    Button component refactored

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowsAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
+
 export class ButtonComponent implements OnInit {
+  @Input() text!:string;
+  @Input() color!:string;
+  @Input() textcolor!:string;
+  @Input() icontype!: IconProp;
+  @Input() iconcolor!: string;
+
+  Quote = faQuoteRight
+  Uparrow = faArrowsAlt
 
   constructor() { }
 
