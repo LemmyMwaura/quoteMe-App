878c6d013fec6cd493efc521e6f25af1b3c01c8f
Lemmy Mwaura <lemminton@gmail.com>
2022-02-08 21:05:26 +0300

    Directive update

--- src/app/directives/most-upvotes.directive.ts
+++ src/app/directives/most-upvotes.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appMostUpvotes]'
@@ -7,22 +7,23 @@ import { Directive, ElementRef, HostListener, HostBinding, Input } from '@angula
 export class MostUpvotesDirective {
   @Input() isMost: any
  
-  constructor(private el:ElementRef, ) { 
+  constructor(private el:ElementRef, private renderer: Renderer2 ) { 
   }
 
   @HostListener('click') onClicks(){
     this.editOtherElem()
-    if (this.isMost.quote){
-      this.el.nativeElement.style.backgroundColor = 'yellow';
-    } else {
-      this.el.nativeElement.style.backgroundColor = 'rgba(255,255,255,0.4)';
-    }
   }
 
-  // @HostBinding('class') class = 'lowest'
-
   editOtherElem(){
-    this.el.nativeElement.style.backgroundColor = 'rgba(255,255,255,0.4)'; 
+    let array:number[] = []
+    this.isMost.quote.forEach((quote:any) => {
+      quote.isMostLiked = false
+      array.push(quote.upvoteNo)
+    })
+
+    let max = Math.max(...array)
+    let i = array.indexOf(max)
+    this.isMost.quote[i].isMostLiked = true;
   }
 
   ngOnInit(){}
